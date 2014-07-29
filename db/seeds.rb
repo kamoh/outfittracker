2fb3e0eedfd767520f2abf4137e84f9802227873
kamoh <ben.serviss@gmail.com>
2014-07-29 17:18:39 -0400

    Adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# ClothingArticles
+
+tops_cat = ClothingCategory.create(
+  category: "tops"
+  )
+
+bottoms_cat = ClothingCategory.create(
+  category: "bottoms"
+  )
+tshirt = ClothingArticle.create(
+  description: "t-shirt", 
+  color: "blue",
+  clothing_category_id: tops_cat.id
+  )
+vest = ClothingArticle.create(
+  description: "vest", 
+  color: "gray",
+  clothing_category_id: tops_cat.id
+  )
+pants = ClothingArticle.create(
+  description: "pants", 
+  color: "brown",
+  clothing_category_id: bottoms_cat.id
+  )
+shorts = ClothingArticle.create(
+  description: "shorts", 
+  color: "green",
+  clothing_category_id: bottoms_cat.id
+  )
+
+casual_outfit = Outfit.create(
+  date: "2013-10-24"
+  )
+
+business_outfit = Outfit.create(
+  date: "2002-02-04"
+  )
+
+casual_outfit.clothing_articles = [tshirt, shorts]
+casual_outfit.save
+business_outfit.clothing_articles = [vest, pants]
+business_outfit.save
