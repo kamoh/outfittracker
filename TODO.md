6c896d9c987b5b7ea03ef1273f3527b8c245944a
analuperez <perez.analuisa@gmail.com>
2014-07-30 14:07:32 -0400

    added show edit destroy controllers for outfits

--- TODO.md
+++ TODO.md
@@ -3,5 +3,12 @@ TODO
 - make the outfit#new page way better
   - nested form?
   - create a new clothing article on the fly?
-  - first select clothing category, then select clothing articles that belong to that category
-  - 
+  
+WEDNESDAY
+  - first select clothing category, then select clothing articles that belong to that 
+  category
+ B - update & destroy & show clothing articles
+  - update & destroy & show outfits
+
+    - update clothing article checkboxes with labels + pictures
+    - outfits showpage - combine pictures of clothing articles
--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -20,6 +20,30 @@ class OutfitsController < ApplicationController
     # redirect_to 'outfits#index'
   end
 
+  def show
+    @outfit = Outfit.find(params[:id])
+  end
+
+  def edit
+    @outfit = Outfit.find(params[:id])
+    @clothing_articles = ClothingArticle.all
+    @friends = Friend.all
+  end
+
+  def update
+    @outfit = Outfit.find(params[:id])
+    @outfit.update(outfit_params)
+    
+    redirect_to @outfit
+  end
+
+  def destroy
+    @outfit = Outfit.find(params[:id])
+    @outfit.destroy
+    redirect_to outfits_path
+  end
+
+
   private
   def outfit_params
     params.require(:outfit).permit(:date, :clothing_article_ids => [], :friend_ids => [])
