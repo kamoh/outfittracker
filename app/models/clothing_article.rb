ae43f7bb534c55cde1a77b63332dec07868bb8d3
kamoh <ben.serviss@gmail.com>
2014-07-30 13:13:10 -0400

    Changes uploaded image paths, formats outfit and clothing article index pages a little

--- app/models/clothing_article.rb
+++ app/models/clothing_article.rb
@@ -4,7 +4,10 @@ class ClothingArticle < ActiveRecord::Base
   has_many :outfits, through: :clothing_article_outfits
 
   # Paperclip
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :photo, :styles => { :medium => "300x300>" }, 
+                    :default_url => "/images/:style/missing.png",
+                    :url => "/assets/clothing_articles/:id/:basename.:extension",
+                    :path => ":rails_root/public/assets/clothing_articles/:id/:basename.:extension"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
 end
