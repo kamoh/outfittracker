89038172c456ddc960cbfa6dc10e2511eb22a65c
analuperez <perez.analuisa@gmail.com>
2014-07-31 17:53:03 -0400

    added top outfits

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@ class User < ActiveRecord::Base
     top = clothing_articles.sort_by(&:times_worn).reverse
     top[0..9]
   end
+
+  def top_outfits
+    all_outfits = outfits.collect do |outfit|
+      outfit.clothing_articles.sort  
+    end
+
+    unique_outfit = all_outfits.collect { |out| { :outfit => out, :count => all_outfits.count(out) }}.uniq { |o| o[:outfit] }.sort_by { |h| h[:count] }.reverse
+  end
 end
+
