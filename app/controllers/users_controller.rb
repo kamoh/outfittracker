3681b2b37ce5b7160e6276097eeae5b474c5cff9
kamoh <ben.serviss@gmail.com>
2014-08-02 21:40:59 -0400

    Initial setup for devise, but devise's user model and our user model are not linked up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ require 'open-uri'
 require 'json'
 
 class UsersController < ApplicationController
+
+  before_action :authenticate_user!
+
   def index
     @users = User.all
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140801141142) do
+ActiveRecord::Schema.define(version: 20140803010337) do
 
   create_table "clothing_article_outfits", force: true do |t|
     t.integer  "clothing_article_id"
@@ -84,7 +84,19 @@ ActiveRecord::Schema.define(version: 20140801141142) do
     t.string   "state"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
