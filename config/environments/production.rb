14151881d754accb141f378e1ae20f308b6a2d9b
kamoh <ben.serviss@gmail.com>
2014-08-18 23:48:21 -0400

    Adds paperclip image storage on s3, re-adds images to Ana's sample account, restricts images to no greater than 300x300 in all pages that have images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,9 +83,9 @@ Rails.application.configure do
 config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['aws_s3_bucket_name'],
-    :access_key_id => ENV['aws_access_key_id'],
-    :secret_access_key => ENV['aws_secret_access_key']
+    :bucket => ENV["aws_s3_bucket_name"],
+    :access_key_id => ENV["aws_access_key_id"],
+    :secret_access_key => ENV["aws_secret_access_key"]
   }
 }
 
