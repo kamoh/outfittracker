2889d3228ba204ec38129db118f4bdb756abdc4a
kamoh <ben.serviss@gmail.com>
2014-09-24 22:48:25 -0400

    Adds defense against a new user signing up with an invalid city or state and breaking the weather API call

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,17 +18,22 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id]) 
     feed = JSON.parse(open(@user.weather_url).read)
-    @current_weather = {
+    if feed["response"]["error"]
+      @current_weather = nil
+    else
+      @current_weather = {
       :summary => feed["current_observation"]["weather"],
       :temp => feed["current_observation"]["temperature_string"],
       :hourly_precip => feed["current_observation"]["precip_1hr_in"],
       :daily_precip => feed["current_observation"]["precip_today_in"],
       :icon_url => feed["current_observation"]["icon_url"]
     }
+    end
   end
 
   def create
     @user = User.new(user_params)
+    raise params.inspect
     @user.save
 
     redirect_to @user
